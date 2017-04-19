4e04b29267b674f83033dd64209650b4f9e74c29
Anatoliy Klots and Ben Metzger <anatoliy@>
2017-04-19 11:53:31 -0700

    method no longer uses reverse

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -8,10 +8,29 @@
 # After specs are passing, build a website, with CSS, where someone can go to check if their words are palindromes.
 #
 # Bonus points: Make your method check a string of words and also a Fixnum.
+# class String
+#   def palindrome?
+#     self_formatted = self.downcase.gsub(/[^a-z0-9]/i, '')
+#     reversed_formatted = self_formatted.reverse()
+#     if reversed_formatted == self_formatted
+#       true
+#     else
+#       false
+#     end
+#   end
+# end
+
 class String
   def palindrome?
     self_formatted = self.downcase.gsub(/[^a-z0-9]/i, '')
-    reversed_formatted = self.downcase.gsub(/[^a-z0-9]/i, '').reverse()
+    self_formatted_array = self_formatted.split("")
+    reversed_array = []
+    self_formatted_array.each() do |letter|
+      reversed_array.unshift(letter)
+    end
+    reversed_formatted = reversed_array.join()
+    puts("word: " + self_formatted)
+    puts("reversed: " + reversed_formatted)
     if reversed_formatted == self_formatted
       true
     else
